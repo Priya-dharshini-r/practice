08f40fab52cca71e2a0b005ae48c38c9b2be19fc
Priya <priyadharshini.r.2017.cse@ritchennai.edu.in>
2021-04-23 18:37:31 +0530

    print 2d array with contineous value using nested while.

---
+++ 2dmat.py
@@ -0,0 +1,18 @@
+m = int(input("Enter m: "))
+n = int(input("Enter n: "))
+
+i = 0
+count = 0
+while i<m:
+	j = 0
+	while j<n:
+		count = count+1
+		print(count,end = " ")
+		j = j+1
+	print()
+	i = i+1
+
+
+# 0 1
+# 2 3
+# 4 5
