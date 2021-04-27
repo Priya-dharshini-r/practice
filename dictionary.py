e795fd907cc518d35ea275a2acfcb483d03ead99
Priya <priyadharshini.r.2017.cse@ritchennai.edu.in>
2021-04-27 14:32:03 +0530

    Finding the frequency of a character in a string using dictionary.

---
+++ dictionary.py
@@ -0,0 +1,18 @@
+string = str(input("Input String : "))
+
+# length = len(string)
+# converted_list = list(string)
+# print(converted_list)
+
+my_dict = {}
+i = 0
+for i in string:
+	if i in my_dict:
+		# print(my_dict)
+		my_dict[i] += 1
+		# print(my_dict)
+	else:
+		my_dict[i] = 1
+		# print(my_dict)
+print(my_dict)
+
