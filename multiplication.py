f1bea7f41cd0a6db557404b1db1b72d463adea02
Priya <priyadharshini.r.2017.cse@ritchennai.edu.in>
2021-05-05 16:10:45 +0530

    Printing multiples of a number upto 10 using for loop.

---
+++ multiplication.py
@@ -0,0 +1,20 @@
+#
+# n = int(input("N: "))
+
+# for i in range(1, 11):
+
+#	product = i*n
+#	print(product)
+# " " "
+# multiplication table
+n = int(input("Enter n "))
+
+i = 1
+
+while i<=10:
+
+	product = i*n
+
+	print(product)
+
+	i = i+1
