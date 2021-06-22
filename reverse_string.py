afb4909d608d9b9239090287bdf2080d14324d64
Priya <priyaramachandran.rp@gmail.com>
2021-06-22 19:20:25 +0530

    Reversed a string by list comprehension.

--- reverse_string.py
+++ reverse_string.py
@@ -1,19 +1,11 @@
-def reverse_words_in_string(string):
-	# Converting string to list using split method
-	string_list = string.split()
-	length = len(string_list)
-	reversed_list = []
+string1 = "Hello"
+string_list = list(string1)
+# print(string_lis(t)
+result = []
+n = len(string_list)
+for i in range(n):
+	result.append(string_list[n-i-1])
+print(result)
+op = "".join(map(str, result))
 
-	for i in range(length):
-		# Grabing the last word in a string
-		result = string_list[length-i-1]
-		reversed_list.append(result)
-	empty_string = " "
-	output = empty_string.join(reversed_list)
-
-	return output
-
-if __name__ == "__main__":
-	string = str(input("Enter string: "))
-	final_output = reverse_words_in_string(string)
-	print(final_output)
+print(op)
