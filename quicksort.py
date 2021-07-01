66e2541a654a7c622c8728a344242b6cc64f2f2f
Priya <priyaramachandran.rp@gmail.com>
2021-07-01 16:41:41 +0530

    Quick sort by lamuto partition.

---
+++ quicksort.py
@@ -0,0 +1,26 @@
+def partition(array, start, end):
+	n = len(array)
+	pivot = array[end]
+	pIndex = start
+	for i in range(start, end):
+		if array[i] <= pivot:
+			array[i], array[pIndex] = array[pIndex], array[i]
+			pIndex += 1
+	array[pIndex], array[end] = array[end], array[pIndex]
+	return pIndex
+
+def quick_sort(array, start, end):
+	if len(array) == 1:
+		return
+
+	if start<end:
+		pi = partition(array, start, end)
+		quick_sort(array, start, pi-1)
+		quick_sort(array, pi+1, end)
+
+
+if __name__ == "__main__":
+	array = [10, 7, 8, 9, 1, 5]
+	n = len(array)
+	p = quick_sort(array, 0, n-1)
+	print(array)
